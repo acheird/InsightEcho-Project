cad58bcb9fcd72121e2884d94f161be73fe43e31
acheird <acheird@gmail.com>
2025-04-21 13:15:49 +0300

    return fetched data from api

--- InsightEcho/src/api/index.js
+++ InsightEcho/src/api/index.js
@@ -3,9 +3,15 @@ import axios from "axios";
 const API_BASE_URL = "http://localhost:5000/api";
 
 export const fetchAnalysis = async () => {
-  return axios.get(`${API_BASE_URL}/analysis`);
+  const res = await axios.get(`${API_BASE_URL}/analysis`);
+  return res.data;
 };
 
 export const submitReview = async (review) => {
   return axios.post(`${API_BASE_URL}/reviews`, review);
+};
+
+export const fetchInsights = async () => {
+  const response = await axios.get(`${API_BASE_URL}/insights`);
+  return response.data;
 };
