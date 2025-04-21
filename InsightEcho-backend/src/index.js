040cfdd18c50e84b4f5546b048224d1d1de50d07
acheird <acheird@gmail.com>
2025-04-21 13:14:25 +0300

    clean code from comments

--- InsightEcho-backend/src/index.js
+++ InsightEcho-backend/src/index.js
@@ -4,14 +4,6 @@ require("dotenv").config();
 
 const app = express();
 
-// const corsOptions = {
-//   origin: "http://localhost:5173",
-//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-//   allowedHeaders: ["Content-Type"],
-// };
-
-// app.use(cors(corsOptions));
-
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
